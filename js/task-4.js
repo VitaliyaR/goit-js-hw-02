5c6155446a7a870f86ef428d229dc655b62b8375
Vitaliia_Rylach <vita.elmura@gmai.com>
2025-03-14 19:14:33 +0100

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -21,10 +21,10 @@ function getShippingCost(country) {
     return `Shipping to ${country} will cost ${price} credits`;
 }
 
-getShippingCost("Australia");
-getShippingCost("Germany");
-getShippingCost("China");
-getShippingCost("Chile");
-getShippingCost("Jamaica");
-getShippingCost("Sweden") ;
+console.log( getShippingCost("Australia"));
+console.log(getShippingCost("Germany")) ;
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden")) ;
 
